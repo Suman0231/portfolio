dee11a5f3f54736cd8bfa4bf5972469b70ec69e9
Suman Adhikari <sumanadhikari0231@outlook.com>
2024-12-13 10:12:54 +0545

    new file:   .gitignore 	new file:   README.md 	new file:   build/asset-manifest.json 	new file:   build/favicon.ico 	new file:   build/index.html 	new file:   build/logo192.png 	new file:   build/logo512.png 	new file:   build/manifest.json 	new file:   build/robots.txt 	new file:   build/static/css/main.546ab255.css 	new file:   build/static/css/main.546ab255.css.map 	new file:   build/static/js/453.0e7cd845.chunk.js 	new file:   build/static/js/453.0e7cd845.chunk.js.map 	new file:   build/static/js/main.41c83015.js 	new file:   build/static/js/main.41c83015.js.LICENSE.txt 	new file:   build/static/js/main.41c83015.js.map 	new file:   build/static/media/Ecommerce.332098341fe34a605e62.png 	new file:   build/static/media/chatbot.3f0da57634126464c83a.jpg 	new file:   build/static/media/education.ce49d99be258f28f3ed5.jpg 	new file:   build/static/media/girlwithMilk-removebg.c3e5e7b3143a70021829.png 	new file:   build/static/media/hotelbooking.739a23bc11d64ca35625.jpg 	new file:   build/static/media/sachin.577bf88a341eeddae112.jpg 	new file:   build/static/media/samjhana.e9098e045635c5ddded2.jpg 	new file:   build/static/media/stockMarker.66ddb594bc91de4d2319.jpg 	new file:   build/static/media/sumina.1aa6f3861d29db3f05a0.jpg 	new file:   build/static/media/suraj.41e3463bdf76bdb170ad.jpg 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/App.test.js 	new file:   src/Components/AboutMe/AboutMe.css 	new file:   src/Components/AboutMe/AboutMe.js 	new file:   src/Components/AboutMe/girlwithMilk-removebg.png 	new file:   src/Components/Admin/ad 	new file:   src/Components/Admin/adminDashboard.js 	new file:   src/Components/Admin/adminLogin.js 	new file:   src/Components/Contact/Contact.js 	new file:   src/Components/Footer/Footer.css 	new file:   src/Components/Footer/Footer.js 	new file:   src/Components/HomePage.js 	new file:   src/Components/Images/Ecommerce.png 	new file:   src/Components/Images/chatbot.jpg 	new file:   src/Components/Images/education.jpg 	new file:   src/Components/Images/girlwithMilk-removebg.png 	new file:   src/Components/Images/hotelbooking.jpg 	new file:   src/Components/Images/sachin.jpg 	new file:   src/Components/Images/samjhana.jpg 	new file:   src/Components/Images/stockMarker.jpg 	new file:   src/Components/Images/sumina.jpg 	new file:   src/Components/Images/suraj.jpg 	new file:   src/Components/MyProject/MyProject.js 	new file:   src/Components/MyTeams/MyTeam.css 	new file:   src/Components/MyTeams/MyTeam.js 	new file:   src/Components/Navbar/Navbar.css 	new file:   src/Components/Navbar/Navbar.js 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/logo.svg 	new file:   src/reportWebVitals.js 	new file:   src/setupTests.js 	new file:   tailwind.config.js

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  // Import Routes and Route
+import HomePage from './Components/HomePage';
+import BasicExample from './Components/Navbar/Navbar';
+import AboutPage from './Components/AboutMe/AboutMe';
+import MyProject from './Components/MyProject/MyProject';
+import MyTeam from './Components/MyTeams/MyTeam';
+import ContactForm from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        {/* Your Navbar */}
+        <BasicExample />
+
+        {/* Define your Routes here */}
+        <Routes>
+          {/* Define a route for HomePage */}
+          <Route path="/homepage" element={<HomePage />} />
+
+          {/* Define routes for other pages */}
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/projects" element={<MyProject />} />
+          <Route path="/team" element={<MyTeam />} />
+          <Route path="/contact" element={<ContactForm />} />
+        </Routes>
+
+        {/* Footer will always be at the bottom */}
+        <HomePage/>
+        <AboutPage/>
+        <MyTeam/>
+        <MyProject/>
+        <ContactForm/>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
